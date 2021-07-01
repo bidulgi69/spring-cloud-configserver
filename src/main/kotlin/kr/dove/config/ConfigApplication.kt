3b82ce9d08813f811eb2e135348f5de2c3adf031
rhyme884 <rhyme884@gmail.com>
2021-07-01 19:27:56 +0900

    config added

--- src/main/kotlin/kr/dove/config/ConfigApplication.kt
+++ src/main/kotlin/kr/dove/config/ConfigApplication.kt
@@ -2,7 +2,9 @@ package kr.dove.config
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
+@EnableConfigServer
 @SpringBootApplication
 class ConfigApplication
 
